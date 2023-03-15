0ddcbdaaed4e308d52d48f6a8c18b340d1334a77
HyungJinHan <han1210_36@naver.com>
2023-03-15 21:51:27 +0900

    Navigation, Sidebar 작업 진행 중
    모바일 환경에 맞게 사이드바 숨김처리 및 별도의 메뉴 버튼을 통해 사이드바 정보 출력 작업 진행
    
    사이드바에 antd의 Tooltip 적용 완료

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Tooltip } from "antd";
 
 import { logo, sun } from "../assets";
+import { navlinks } from "../constants";
+// 위의 파일 내용을 통해 이미지와 링크 url을 불러와서 사용
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
   <div
@@ -10,6 +13,7 @@ const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
     } flex justify-center items-center ${
       !disabled && "cursor-pointer"
     } ${styles}`}
+    onClick={handleClick}
   >
     {!isActive ? (
       <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
@@ -32,6 +36,42 @@ const Sidebar = () => {
       <Link to="/">
         <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
       </Link>
+
+      <div className="flex-1 flex flex-col justify-between items-center bg-[#1c1c24] rounded-[20px] w-[76px] py-4 mt-12">
+        <div className="flex flex-col justify-center items-center gap-3">
+          {/* navlinks는 배열 형태로 만들어졌기 때문에 map 함수 적용이 가능 */}
+
+          {navlinks.map((link) => (
+            <Tooltip
+              key={link.name}
+              placement="right"
+              title={link.name.replace(/^[a-z]/, (char) => char.toUpperCase())}
+              // 첫 글자 대문자 작업
+              color={"#4acd8d"}
+              overlayInnerStyle={{
+                color: "white",
+                fontWeight: "bold",
+                textShadow: "0 0 5px black",
+              }}
+            >
+              <div key={link.name}>
+                <Icon
+                  key={link.name}
+                  {...link}
+                  isActive={isActive}
+                  handleClick={() => {
+                    if (!link.disabled) {
+                      setIsActive(link.name);
+                      navigate(link.link);
+                    }
+                  }}
+                />
+              </div>
+            </Tooltip>
+          ))}
+        </div>
+        <Icon styles="bg-[#1c1c24] shadow-secondary" imgUrl={sun} />
+      </div>
     </div>
   );
 };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
-
 import App from "./App";
 import "./global.css";
 
