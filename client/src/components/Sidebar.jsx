e61e41056b502607c6f1a88f56420088dafaf251
HyungJinHan <han1210_36@naver.com>
2023-03-15 18:40:03 +0900

    기본적인 세팅 진행 및 사이드바 스타일링 진행 중

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { logo, sun } from "../assets";
+
+const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
+  <div
+    className={`w-[48px] h-[48px] rounded-[10px] ${
+      isActive && isActive === name && "bg-[#2c2f32]"
+    } flex justify-center items-center ${
+      !disabled && "cursor-pointer"
+    } ${styles}`}
+  >
+    {!isActive ? (
+      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+    ) : (
+      <img
+        src={imgUrl}
+        alt="fund_logo"
+        className={`w-1/2 h-1/2 ${isActive !== name && "grayscale"}`}
+      />
+    )}
+  </div>
+);
+
+const Sidebar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+
+  return (
+    <div className="flex justify-between items-center flex-col sticky top-5 h-[93vh]">
+      <Link to="/">
+        <Icon styles="w-[52px] h-[52px] bg-[#2c2f32]" imgUrl={logo} />
+      </Link>
+    </div>
+  );
+};
+
+export default Sidebar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+
 import App from "./App";
+import "./global.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-    <Router>
+  <ThirdwebProvider activeChain={ChainId.Goerli}>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </ThirdwebProvider>
 );
