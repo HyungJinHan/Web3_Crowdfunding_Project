cbb54f186711a1e185eb26a70265e52b6a5d980b
HyungJinHan <han1210_36@naver.com>
2023-03-17 01:50:06 +0900

    로딩 화면 수정

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -3,7 +3,7 @@ import { loader } from "../assets";
 
 const Loader = () => {
   return (
-    <div className="fixed inest-0 z-10 h-screen bg-[rgba(0, 0, 0, 0.7)] flex items-center justify-center flex-col">
+    <div className="fixed inset-0 z-10 h-screen bg-[#000000a6] flex items-center justify-center flex-col">
       <img
         src={loader}
         alt="loading"
